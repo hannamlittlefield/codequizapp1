03e9a9073a3f65604d211fbcbd2c092783210c9e
Hanna Littlefield <hannamlittlefield@gmail.com>
2020-12-14 13:46:45 -0500

    creating home icon to click back

--- src/AppBar.js
+++ src/AppBar.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import {AppBar, Toolbar, IconButton, Typography} from '@material-ui/core'
+import Mainpage from './Mainpage';
+import ReactDOM from 'react-dom'
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
 
 
 function Appbar() {
+  const handleOnClick = () => {
+    ReactDOM.render(
+      <Mainpage />, 
+    document.getElementById('root')
+    )
+  }
     return (
 <AppBar position="static" className="Appbar">
   <Toolbar>
     <IconButton edge="start" color="inherit" aria-label="menu">
     </IconButton>
     <Typography variant="h3" className="TitleText">
-      Code Quiz App
+      <HomeOutlinedIcon fontSize="large" onClick={handleOnClick}/>Code Quiz App
     </Typography>
   </Toolbar>
 </AppBar>
