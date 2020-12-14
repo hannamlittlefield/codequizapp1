58ca4be60ec5923619ba6619fe41ae2e58775dc0
Hanna Littlefield <hannamlittlefield@gmail.com>
2020-12-14 13:47:23 -0500

    centering question card on page

--- src/QuizPage.js
+++ src/QuizPage.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import AppBar from './AppBar'
 import QuestionCard from './QuestionCard';
+import {Container} from '@material-ui/core';
 
 export default function QuizPage(){
     return(
     <React.Fragment>
     <AppBar/>
-    <QuestionCard/>
+    <Container maxWidth="sm" className="marginspace">
+    <QuestionCard
+    justify="center"/>
+    </Container>
     </React.Fragment>
     )
 }
