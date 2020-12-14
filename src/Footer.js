190037a5bad68e94cc673e0b0439c66b1a6234ce
Hanna Littlefield <hannamlittlefield@gmail.com>
2020-12-14 13:46:08 -0500

    creating a footer

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import { AppBar } from '@material-ui/core'
+import React from 'react'
+import Appbar from '@material-ui/core';
+
+
+export default function Footer(){
+    return (
+        <AppBar className="Footer" position="sticky">'This is the footer la la la'</AppBar>
+    );
+}
