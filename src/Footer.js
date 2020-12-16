e40a2d85e8e7cacead230a9cd813cf8c1f6e2181
Hanna Littlefield <hannamlittlefield@gmail.com>
2020-12-16 16:50:52 -0500

    sticking footer to the bottom of the page

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,8 @@
-import { AppBar } from '@material-ui/core'
 import React from 'react'
 
 
 export default function Footer(){
     return (
-        <AppBar className="Footer" position="sticky">'This is the footer la la la'</AppBar>
+        <footer className="Footer" position="sticky">&copy; 2020 Developed by Hanna Littlefield</footer>
     );
 }
